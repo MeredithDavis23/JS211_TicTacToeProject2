693061439bb8aef20fbd9775edeb4a9d04955856
Meredith Davis <meredithd23@yahoo.com>
2020-05-10 13:13:59 -0500

    winner

--- main.js
+++ main.js
@@ -33,27 +33,47 @@ const printBoard = () => {
 }
 
 const horizontalWin = () => {
-  if(board[0][0] === "X" && board[0][1] === "X" && board[0][2] === "X" || board[0][0] === "O" && board[0][1] === "O" && board[0][2] === "O");
-  if(board[1][0] === "X" && board[1][1] === "X" && board[1][2] === "X" || board[1][0] === "O" && board[1][1] === "O" && board[1][2] === "O");
-  if(board[2][0] === "X" && board[2][1] === "X" && board[2][2] === "X" || board[2][0] === "O" && board[2][1] === "O" && board[2][2] === "O");
+  if((board[0][0] == "X" && board[0][1] == "X" && board[0][2] == "X") || (board[0][0] == "O" && board[0][1] == "O" && board[0][2] == "O")) {
+    return true
+  }
+  
+  if((board[1][0] == "X" && board[1][1] == "X" && board[1][2] == "X") || (board[1][0] == "O" && board[1][1] == "O" && board[1][2] == "O")) {
+    return true
+  }
+    
+  if((board[2][0] == "X" && board[2][1] == "X" && board[2][2] == "X") || (board[2][0] == "O" && board[2][1] == "O" && board[2][2] == "O")) {
+    return true 
+  }
+
 }
 
 
 const verticalWin = () => {
-  if(board[0][0] === "X" && board[1][0] === "X" && board[2][0] === "X" || board[0][0] === "O" && board[1][0] === "O" && board[2][0] === "O");
-  if(board[0][1] === "X" && board[1][1] === "X" && board[2][1] === "X" || board[0][1] === "O" && board[1][1] === "O" && board[2][1] === "O");
-  if(board[0][2] === "X" && board[1][2] === "X" && board[2][2] === "X" || board[0][2] === "O" && board[1][2] === "O" && board[2][2] === "O");
+  if((board[0][0] == "X" && board[1][0] == "X" && board[2][0] == "X") || (board[0][0] == "O" && board[1][0] == "O" && board[2][0] == "O")) {
+    return true 
+  }
+  if((board[0][1] == "X" && board[1][1] == "X" && board[2][1] == "X") || (board[0][1] == "O" && board[1][1] == "O" && board[2][1] == "O")) {
+    return true 
+  }
+  if((board[0][2] == "X" && board[1][2] == "X" && board[2][2] == "X") || (board[0][2] == "O" && board[1][2] == "O" && board[2][2] == "O")) {
+    return true 
+  }
 }
 
 const diagonalWin = () => {
-  if(board[0][0] === "X" && board[1][1] === "X" && board[2][2] === "X" || board[0][0] === "O" && board[1][1] === "O" && board[2][2] === "O");
-  if(board[0][2] === "X" && board[1][1] === "X" && board[2][0] === "X" || board[0][2] === "O" && board[1][1] === "O" && board[2][0] === "O");
+  if((board[0][0] == "X" && board[1][1] == "X" && board[2][2] == "X") || (board[0][0] == "O" && board[1][1] == "O" && board[2][2] == "O")) {
+    return true 
+  }
+  if((board[0][2] == "X" && board[1][1] == "X" && board[2][0] == "X") || (board[0][2] == "O" && board[1][1] == "O" && board[2][0] == "O")) {
+    return true 
+  }
 }
 
 const checkForWin = () => {
   // Your code here call each of the check for types of wins
   if(horizontalWin() || verticalWin() || diagonalWin()) {
-    return ("You Won!")
+    console.log("You Won!")
+    return true
   } else {
     ticTacToe()
   }
@@ -62,13 +82,10 @@ const checkForWin = () => {
 const ticTacToe = (row, column) => {
   // Your code here to place a marker on the board
   // then check for a win
+  board[row][column] = playerTurn;
   if(playerTurn === "X") {
-    return "O"
-  } else if(playerTurn !== "X") {
-    return "X"
-  } else {
-    return checkForWin()
-  }
+    playerTurn = "O";
+  } else(playerTurn = "X") 
 }
 
 const getPrompt = () => {
